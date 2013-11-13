11fc9ae188b26506f8f0e97bfc2c03c5d4e7ac5f
Michael 'devbug' Williams <m.t.williams@live.com>
2013-11-12 17:24:13 -0800

    Made into dotfiles (rather than just Sublime Text configuration). Moved Sublime Text 3 into sublime-text-3. Added noct/sublime-c99. Added noct/sublime-cpp11. Added install.bat.

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/Color Schemes by devbug" "%cd%/sublime-text-3/Packages/Color Schemes by devbug"
+mklink "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/User/Preferences.sublime-settings" "%cd%/sublime-text-3/Packages/User/Preferences.sublime-settings"
+mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/sublime-c99" "%cd%/sublime-text-3/Packages/sublime-c99"
+mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/sublime-cpp11" "%cd%/sublime-text-3/Packages/sublime-cpp11"
