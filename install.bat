4fbc50ced2f7713aab2ede3ae8e3f84a1b2d3406
Michael 'devbug' Williams <m.t.williams@live.com>
2014-03-13 21:58:03 -0700

    Switched to Spacegray theme.

--- install.bat
+++ install.bat
@@ -1,6 +1,13 @@
 @echo off
-mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/Color Schemes by devbug" "%cd%/sublime-text-3/Packages/Color Schemes by devbug"
+
+echo Setting up Sublime Text 3...
+echo Make sure to install Pacakge Control,
+echo  and to install "Theme - Spacegray"
+
 mklink "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/User/Preferences.sublime-settings" "%cd%/sublime-text-3/Packages/User/Preferences.sublime-settings"
 mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/sublime-c99" "%cd%/sublime-text-3/Packages/sublime-c99"
 mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/sublime-cpp11" "%cd%/sublime-text-3/Packages/sublime-cpp11"
+
+echo Setting up Terminal
+
 "terminal/base16-ocean.reg"
