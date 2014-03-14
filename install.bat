4760f7a0741bfee4ccb1f639c042de0d8c035b5d
Michael 'devbug' Williams <m.t.williams@live.com>
2014-03-13 17:16:25 -0700

    Added terminal colour scheme (base16-ocean)

--- install.bat
+++ install.bat
@@ -3,3 +3,4 @@ mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/Color Sch
 mklink "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/User/Preferences.sublime-settings" "%cd%/sublime-text-3/Packages/User/Preferences.sublime-settings"
 mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/sublime-c99" "%cd%/sublime-text-3/Packages/sublime-c99"
 mklink /D "C:/Users/%USERNAME%/AppData/Roaming/Sublime Text 3/Packages/sublime-cpp11" "%cd%/sublime-text-3/Packages/sublime-cpp11"
+"terminal/base16-ocean.reg"
